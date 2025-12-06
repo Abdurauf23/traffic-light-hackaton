4b34e47fc50fa9b539dae8d09047bad79a72ef01
Abdurauf Vakhobov <abduraufv23@gmail.com>
2025-12-06 20:59:24 +0500

    Initial commit

---
+++ src/main/java/uz/traffic/light/hackaton/TrafficLightHackatonApplication.java
@@ -0,0 +1,13 @@
+package uz.traffic.light.hackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficLightHackatonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficLightHackatonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/traffic/light/hackaton/TrafficLightHackatonApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.traffic.light.hackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficLightHackatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
