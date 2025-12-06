a99ca7187414ee42f8cd4c83c52186bb375f7f7a
Abdurauf Vakhobov <abduraufv23@gmail.com>
2025-12-06 22:20:52 +0500

    add chat ui controller and add links to the other pages in the main page

---
+++ src/main/java/uz/traffic/light/hackaton/controller/chat/ChatUIController.java
@@ -0,0 +1,14 @@
+package uz.traffic.light.hackaton.controller.chat;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatUIController {
+    @GetMapping
+    public String chatPage(){
+        return "chat";
+    }
+}
