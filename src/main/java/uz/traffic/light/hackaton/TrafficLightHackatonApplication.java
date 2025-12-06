29360aa68fe0a1eec228527e91d1240ffd4b55a3
Abdurauf Vakhobov <abduraufv23@gmail.com>
2025-12-06 21:17:15 +0500

    Main page fix

--- src/main/java/uz/traffic/light/hackaton/TrafficLightHackatonApplication.java
+++ src/main/java/uz/traffic/light/hackaton/TrafficLightHackatonApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TrafficLightHackatonApplication {
-
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(TrafficLightHackatonApplication.class, args);
     }
-
 }
---
+++ src/main/java/uz/traffic/light/hackaton/controller/chat/ChatAPIController.java
@@ -0,0 +1,8 @@
+package uz.traffic.light.hackaton.controller.chat;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatAPIController {
+
+}
